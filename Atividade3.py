0cd94837cb6261ca93de4215f20112c05887c922
kaykymallmann <130004347+kaykymallmann@users.noreply.github.com>
2025-03-19 21:23:08 -0300

    Update Atividade3.py

--- Atividade3.py
+++ Atividade3.py
@@ -1,9 +1,12 @@
-def ler_arquivo(nome_arquivo):
+def ler_arquivo(nome_arquivo): 
     try:
-        arquivo = open(nome_arquivo, "r")
-        conteudo = arquivo.read()
-        arquivo.close()
+        with open(nome_arquivo, "r") as arquivo:
+            conteudo = arquivo.read()
         return conteudo
-    except:
-        print("Erro ao ler arquivo") 
+    except FileNotFoundError:
+        print(f"Erro: O arquivo '{nome_arquivo}' não foi encontrado.")
+    except IOError:
+        print("Erro ao ler o arquivo.")
+    return None
+
 print(ler_arquivo("dados.txt"))
